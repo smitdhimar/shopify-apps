11d0a1800caf9a9a7503d7e13bb608011c9a2311
Brijesh Patadiya <brijesh.patadiya@devxconsultancy.com>
2025-03-07 12:25:03 +0530

    add auth role and policy

--- cognitoPoolIdentity/main.tf
+++ cognitoPoolIdentity/main.tf
@@ -65,6 +65,49 @@ resource "aws_iam_policy" "unauthenticated_policy" {
   })
 }
 
+resource "aws_iam_role" "authenticated" {
+  name = "authenticated-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = "cognito-identity.amazonaws.com"
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "cognito-identity.amazonaws.com:aud" = aws_cognito_identity_pool.personalizer_identity_pool.id
+          }
+          "ForAnyValue:StringLike" = {
+            "cognito-identity.amazonaws.com:amr" = "authenticated"
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "authenticated" {
+  name = "authenticated-policy"
+  role = aws_iam_role.authenticated.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "cognito-sync:*",
+          "cognito-identity:*"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
 resource "aws_iam_role_policy_attachment" "unauthenticated_policy_attach" {
   role       = aws_iam_role.unauthenticated_role.name
   policy_arn = aws_iam_policy.unauthenticated_policy.arn
@@ -75,5 +118,6 @@ resource "aws_cognito_identity_pool_roles_attachment" "personalizer_identity_poo
 
   roles = {
     unauthenticated = aws_iam_role.unauthenticated_role.arn
+    authenticated  = aws_iam_role.authenticated.arn
   }
 }
